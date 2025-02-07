0d93ba5cbab395f6afadd579ba53c5b1b5314dd3
Tans-jpg <tanvi.agarwal@kalvium.community>
2025-02-07 22:53:14 +0530

    mongoDB connected

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const connectDB = require("./connectDB"); // Import connectDB
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB Atlas
-
+connectDB();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the Food Shape Matcher");
+  res.send("Welcome to the  Food Shape Matcher 4000");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
