8172a4dd6236fc0e3d35e060052ada3c60fd0a9c
Tans-jpg <tanvi.agarwal@kalvium.community>
2025-02-11 16:19:27 +0530

    api testing

--- server.js
+++ server.js
@@ -3,9 +3,28 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./connectDB"); // Import connectDB
 
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const shapeRoutes = require('./routes');
+
 const app = express();
+
+
+app.use(bodyParser.json());
+app.use('/api', shapeRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+
+
 app.use(express.json());
 app.use(cors());
+
 
 // Connect to MongoDB Atlas
 connectDB();
@@ -15,4 +34,4 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console. log(`Server is running on http://localhost:${process.env.PORT}`))
