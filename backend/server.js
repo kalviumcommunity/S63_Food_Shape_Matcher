7a2fd064e0a14d45b318bc5cb64723de1cff450d
Tanvi_Agarwal <tanvi.agarwal@kalvium.community>
2025-06-24 02:45:43 +0530

    Update server.js(2)

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const connectDB = require("./connectDb"); // Fixed case sensitivity
 const bodyParser = require('body-parser');
 // const shapeRoutes = require('./routes');
-const routes = require('./routes'); 
+// const routes = require('./routes'); 
 // const Entity = require('./models/entity'); // Import the Entity model
 const app = express();
 
@@ -14,11 +14,11 @@ app.use(cookieParser());
 
 app.use(express.json());
 app.use(cors());
-app.use('/api',routes);n
+// app.use('/api',routes);n
 
 // Connect to MongoDB Atlas
 connectDB();
-let entities = [];
+// let entities = [];
 
 
 const PORT = process.env.PORT || 4000;
